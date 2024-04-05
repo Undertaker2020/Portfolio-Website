64ece7837ea969d7f9f53745e193069c11f47a14
Ilya <gashimovilya2018@gmail.com>
2024-04-05 21:08:19 +0300

    Commit Version 0.1.0

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 import {LogoBehance, LogoDeviantart, LogoDribbble, LogoFigma} from "react-ionicons";
 
 const Hero = () => {
-    return(
+    return (
         <div className="hero w-full h-screen flex items-center justify-center
                         md:px-[250px] px-5 relative">
             <div className="flex w-full flex-col gap-8 z-50 items-center
@@ -15,19 +15,21 @@ const Hero = () => {
                 </span>
                 <p className="text-orange-200 md:max-w-[600px] max-w-[450px]
                                 leading-7 text-center">
-                    Lalalalalalalalalalalalalalalalalalalalalalalalalalalalalal
-                    alalallalalalalalalalalalalalalalalalalalalalalalalalalalalala
-                    alalalalalalalalalalalalalalalalalalalalalaallalalalalallalalal
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
                 </p>
                 <div className="">
-                    <LogoDribbble />
-                    <LogoDeviantart />
-                    <LogoBehance />
-                    <LogoFigma />
+                    <LogoDribbble/>
+                    <LogoDeviantart/>
+                    <LogoBehance/>
+                    <LogoFigma/>
                 </div>
             </div>
         </div>
-    )
+    );
 };
 
 export default Hero;
